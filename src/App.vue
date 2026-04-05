f1a4ff30421f16b040db71a235669f8757b075d7
ziyu-0123 <ziyuliu123@qq.com>
2026-04-05 18:22:03 +0800

    feat: init project & complete basic Button component

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import { ref, onMounted } from 'vue'
+import type { ButtonInstance } from './components/Button/types'
+import Button from './components/Button/Button.vue'
+const buttonRef = ref<ButtonInstance | null>()
+onMounted(()=>{
+  if(buttonRef.value){
+    console.log('buttonRef', buttonRef.value.ref)
+  }
+})
+</script>
+ 
+<template>
+  <Button type="primary" plain disabled ref="buttonRef">Test Button</Button>
+</template>
+
+<style scoped></style>
---
+++ src/components/Button/Button.vue
@@ -0,0 +1,42 @@
+<template>
+  <button
+    ref="_ref"
+    class="nano-button"
+    :class="{
+      [`nano-button--${type}`]:type,
+      [`nano-button--${size}`]:size,
+      'is-plain':plain,
+      'is-round':round,
+      'is-circle':circle,
+      'is-disabled':disabled
+    }"
+    :disabled="disabled"
+    :autofocus="autofocus"
+    :type="nativeType"
+    >
+    <span>
+      <slot></slot>
+    </span>
+  </button>
+</template>
+
+<script lang="ts">
+export default{
+  name: 'NanoButton'
+}
+</script>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+import type { ButtonProps } from './types'
+
+withDefaults(defineProps<ButtonProps>(), {
+  nativeType: 'button'
+})
+
+const _ref = ref<HTMLButtonElement>()
+
+defineExpose({
+  ref: _ref
+})
+</script>
