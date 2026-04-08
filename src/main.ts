a6706716fa6e29df25a35e77402f41e73faf4840
ziyu-0123 <ziyuliu123@qq.com>
2026-04-08 12:13:46 +0800

    feat: add unit tests

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,6 @@ import { library } from '@fortawesome/fontawesome-svg-core'
 import { fas } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import App from './App.vue'
-
-library.add(fas)
 import './styles/index.css'
-
+library.add(fas)
 createApp(App).component('font-awesome-icon', FontAwesomeIcon).mount('#app')
